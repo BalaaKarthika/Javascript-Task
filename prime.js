9dbc55bca6edc1eeb9add0f63ee92a668032d270
BalaaKarthika <157867163+BalaaKarthika@users.noreply.github.com>
2024-05-11 22:44:11 +0530

    Add files via upload

---
+++ prime.js
@@ -0,0 +1,12 @@
+const prime=prompt("enter the prime number")
+for(i=0;i<100;i++)
+{
+    if(prime%2==0)
+    {
+        console.log("the number"+prime+" is a prime number");
+    }
+    else{
+        console.log("the number"+prime+" is a not prime number");   
+    }
+}
+
