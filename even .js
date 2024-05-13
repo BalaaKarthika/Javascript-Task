8174807fd06e4c4ea4c2c011db21e1e4b4bd035d
BalaaKarthika <157867163+BalaaKarthika@users.noreply.github.com>
2024-05-13 23:33:45 +0545

    Add files via upload

---
+++ even .js
@@ -0,0 +1,30 @@
+
+function on(){
+    document.getElementById('demo').innerHTML='on'
+}
+function over(){
+    document.getElementById('demo').innerHTML='over'
+}
+
+function up(){
+    document.getElementById('demo').innerHTML='up'
+}
+function down(){
+    document.getElementById('demo').innerHTML='down'
+}
+function leave(){
+    document.getElementById('demo').innerHTML='leave'
+}
+function enter(){
+    document.getElementById('demo').innerHTML='enter'
+}
+function move(){
+    document.getElementById('demo').innerHTML='move'
+}
+
+
+function move(){
+  let a =  Math.floor(Math.random()*100)   
+  document.getElementById('demo').innerHTML= a 
+
+}
