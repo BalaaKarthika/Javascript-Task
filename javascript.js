7851fcc45b5800d97f9ec5b3aea67e9ce20266bb
BalaaKarthika <157867163+BalaaKarthika@users.noreply.github.com>
2024-09-08 18:09:31 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,59 @@
+//var- old
+//let-new
+//let name = "karthika";
+/*const name  = "karthika";
+const dob = "09-02-2003";
+let msg = " Hello my name is "+  name +"& i born on " + dob + " i am learning javascript";
+alert(msg);
+
+console.log(msg);
+
+
+ rules
+ 1.no javascript key words;
+ 2.should start with number;
+ 3.No space & No -;
+ 4. Its Case Sensitive;
+ 5.Use Meaningful names
+*/
+ //  Data Types
+//let firstName = " Karthika"; // String
+
+//let Age = 21;  // integer
+
+//let isYoung =  true;  // Boolean
+
+//let lastName = undefined;
+
+// let trophy = null; // undifind
+ 
+ // Objects
+/*
+ let person = {
+    name: "kamalesh",
+    Age: 24,
+    isAlive: "true",
+    gender: "Male",
+    Address:"Chennai",
+    siblings: {
+        brother: "kamalesh Balakrihnan",
+        sister: " karthika Balakrishnan"
+
+    }
+
+ } */ 
+ //person.Age = 25
+ //Dot Notation
+
+//console.log(person);
+//Bracket Notation
+//console.log(person);
+//{}- object
+//[]- array
+let favColors = ['red','blue','green','yellow']
+
+favColors[5] = 'White'
+
+console.log(favColors.length);
+
+ 
